ca40e5993c929a9424e1779b3e2a299456258c46
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-09 16:23:34 +0000

    Instantiating a Class

--- class_definition.php
+++ class_definition.php
@@ -4,27 +4,16 @@
 class Cars {
 
 // A function in a class is called a method
-
 function greeting(){
+    echo "Hello Student";
+}
 
 }
 
+$bmw = new Cars();
+$mercedes = new Cars();
 
 
-
-}
-
-// See All Classes that a available
-$my_classes = get_declared_classes();
-
-foreach ($my_classes as $class) {
-    echo $class;
-}
-
-$my_methods = get_class_methods('Cars');
-
-foreach ($my_methods as $method) {
-    echo $method . "br";
-}
-
+$bmw->greeting();
+$mercedes->greeting();
 ?>
