f4174d567daa3a7a82804ed6b6c6facb7c692144
Stephen Jackson <jjackson19862017@gmail.com>
2020-11-09 18:26:02 +0000

    Static Parent Reference

---
+++ static_reference.php
@@ -0,0 +1,16 @@
+<?php
+// Making a Class
+class Cars 
+{
+	static $wheel_count = 4;
+	static $door_count = 4; 
+	// A function in a class is called a method
+	static function car_detail()
+	{
+	echo self::wheel_count . "<br>";
+	echo self::door_count . "<br>";
+	}
+}
+echo Cars::$door_count;  // Calling a Static Method
+//Cars::car_detail();
+?>
